465d88f923e2905a03ca741665c24c1c18b9e6d2
aplanas1 <aplanas@elpuig.xeill.net>
2022-05-08 12:55:19 +0200

    Video 13

--- Player/Player.gd
+++ Player/Player.gd
@@ -18,9 +18,11 @@ var roll_vector = Vector2.LEFT
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var sworHitbox = $HitboxPivot/SwordHitbox
 
 func _ready():
 	animationTree.active = true
+	sworHitbox.knockback_vector = roll_vector
 
 func _process(delta):
 	match state:
@@ -39,6 +41,7 @@ func move_state(delta):
 	
 	if input_vector != Vector2.ZERO:
 		roll_vector = input_vector
+		sworHitbox.knockback_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
