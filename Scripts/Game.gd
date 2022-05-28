8086ea96fb0c877fa789e442c02c5e63c391ac0e
aplanas1 <aplanas@elpuig.xeill.net>
2022-05-28 11:07:38 +0200

    Quiero morir

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 onready var _ui_inventory_screen := $CanvasLayer/UI/UIInventoryScreen
+onready var _ui_stats_screen := $CanvasLayer/UI/UIStatsScreen
 onready var _ui_audio_player := $CanvasLayer/UI/AudioStreamPlayer
 
 export(AudioStream) var audio_stream_open_inventory
@@ -10,17 +11,32 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("open_inventory"):
 		if GameState.state == Types.GameStates.Inventory:
 			_ui_inventory_screen.set_visible(false)
+			_ui_stats_screen.set_visible(false)
 			GameState.state = Types.GameStates.Active
 			_play_ui_audio(audio_stream_close_inventory)
 		else:
 			show_inventory()
+	if event.is_action_pressed("open_stats"):
+		if GameState.state == Types.GameStates.Inventory:
+			_ui_stats_screen.set_visible(false)
+			_ui_inventory_screen.set_visible(false)
+			GameState.state = Types.GameStates.Active
+			_play_ui_audio(audio_stream_close_inventory)
+		else:
+			show_character()
 
 
 func show_inventory() -> void:	
+	_ui_stats_screen.set_visible(false)
 	_ui_inventory_screen.set_visible(true)
 	GameState.state = Types.GameStates.Inventory
 	_play_ui_audio(audio_stream_open_inventory)
 
+func show_character() -> void:	
+	_ui_inventory_screen.set_visible(false)
+	_ui_stats_screen.set_visible(true)
+	GameState.state = Types.GameStates.Inventory
+	_play_ui_audio(audio_stream_open_inventory)
 
 func _play_ui_audio(stream : AudioStream) -> void:
 	if not stream: return
